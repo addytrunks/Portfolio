8c82f0f27b476008105aae89a159eee11898eaaa
Adhithya Srivatsan <addytrunks@gmail.com>
2023-12-03 20:07:02 +0530

    feat: header (w animation)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,28 @@
+"use client";
+
+import { motion } from "framer-motion";
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 
 const Header = () => {
   return (
     <header className="sticky top-0 p-4 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        className="flex items-center"
+      >
         <SocialIcon
           url="https://www.instagram.com/addy__srivats/"
           fgColor="gray"
@@ -20,17 +38,34 @@ const Header = () => {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+      </motion.div>
 
-      <div className="flex items-center text-gray-200 cursor-pointer">
+      <motion.div
+        initial={{
+          x: 500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+          x: 0,
+          opacity: 1,
+          scale: 1,
+        }}
+        transition={{
+          duration: 0.5,
+        }}
+        className="flex items-center text-gray-200 cursor-pointer"
+      >
         <SocialIcon
           className="cursor-pointer"
-          network="email"  
+          network="email"
           fgColor="gray"
           bgColor="transparent"
         />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">Get in touch</p>
-      </div>
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get in touch
+        </p>
+      </motion.div>
     </header>
   );
 };
