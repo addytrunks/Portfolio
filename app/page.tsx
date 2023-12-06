671ffece6ab0a5026b81d5c9846fa3117d8ccf5c
Adhithya Srivatsan <addytrunks@gmail.com>
2023-12-06 19:09:43 +0530

    feat: scrollbar + fix hydration error

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen overflow-y-scroll z-0 snap-mandatory snap-y">
+    <div className="bg-[rgb(36,36,36)] text-white h-screen overflow-y-scroll z-0 snap-mandatory snap-y overflow-x-hidden scrollbar-track-gray-400/20 scrollbar-thumb-indigo-400 scrollbar-thin">
       <Header />
 
       <section id="hero" className="snap-center">
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { motion } from "framer-motion";
+import Link from "next/link";
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 
 const Header = () => {
+
+  const [isMounted, setIsMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if(!isMounted) return null;
+
   return (
     <header className="sticky top-0 p-4 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
@@ -40,32 +50,34 @@ const Header = () => {
         />
       </motion.div>
 
-      <motion.div
-        initial={{
-          x: 500,
-          opacity: 0,
-          scale: 0.5,
-        }}
-        animate={{
-          x: 0,
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 0.5,
-        }}
-        className="flex items-center text-gray-200 cursor-pointer"
-      >
-        <SocialIcon
-          className="cursor-pointer"
-          network="email"
-          fgColor="gray"
-          bgColor="transparent"
-        />
-        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
-          Get in touch
-        </p>
-      </motion.div>
+      <Link href="#contact">
+        <motion.div
+          initial={{
+            x: 500,
+            opacity: 0,
+            scale: 0.5,
+          }}
+          animate={{
+            x: 0,
+            opacity: 1,
+            scale: 1,
+          }}
+          transition={{
+            duration: 0.5,
+          }}
+          className="flex items-center text-gray-200 cursor-pointer"
+        >
+          <SocialIcon
+            className="cursor-pointer"
+            network="email"
+            fgColor="gray"
+            bgColor="transparent"
+          />
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get in touch
+          </p>
+        </motion.div>
+      </Link>
     </header>
   );
 };
