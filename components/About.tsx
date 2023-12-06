6b77cf2c86ca306b36308cf684f2f39029c011fb
Adhithya Srivatsan <addytrunks@gmail.com>
2023-12-06 17:14:55 +0530

    fix: positioning

--- components/About.tsx
+++ components/About.tsx
@@ -17,7 +17,7 @@ const About = () => {
       }}
       className="h-screen flex flex-col relative text-center md:text-left md:flex-row max-w-7xl px-10 mx-auto justify-evenly items-center"
     >
-      <h3 className="absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
       <motion.img
@@ -34,7 +34,7 @@ const About = () => {
           duration: 0.5,
         }}
         src="/my.jpg"
-        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[450px] xl:h-[550px]"
+        className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-96 xl:w-[450px] xl:h-[500px]"
       />
 
       <div className="space-y-10 px-0 md:px-10">
