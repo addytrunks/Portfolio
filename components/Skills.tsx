cce5254efbf4be71ee5f6f5a917abf895df53bbd
Adhithya Srivatsan <addytrunks@gmail.com>
2023-12-08 11:21:03 +0530

    feat: add padding

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -8,14 +8,14 @@ interface Props{
 
 const Skills = ({skills}:Props) => {
   return (
-    <div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 mx-auto min-h-screen justify-center xl:space-y-0 items-center">
+    <div className="flex relative flex-col text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10  min-h-screen justify-center xl:space-y-0 mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
         Skills
       </h3>
 
       <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-600 text-sm">Hover over a skill for current proficiency</h3>
 
-      <div className="grid grid-cols-4 gap-5">
+      <div className="grid grid-cols-4 gap-5 xl:pt-20">
             {skills?.slice(0,skills.length/2).map((skill) => (
               <Skill key={skill._id} progress={skill.progress} imageUrl={urlFor(skill?.image).url()}/>
             ))}
