04bc19c050fa2b730f71c9fe13ac4fc1110fc061
Adhithya Srivatsan <addytrunks@gmail.com>
2023-12-09 22:02:39 +0530

    feat: change hover size

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -27,7 +27,7 @@ const Skill = ({directionLeft,imageUrl,progress,noAnimate}:SkillProps) => {
             src={imageUrl}
             className="rounded-full border-gray-500 border object-cover h-10 w-10 xl:h-24 xl:w-24 filter group-hover:grayscale transition duration-300 ease-in-out"
         />
-        <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white/50 h-20 w-20 xl:w-24 xl:h-24 rounded-full z-0">
+        <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white/50 h-10 w-10 xl:w-24 xl:h-24 rounded-full z-0">
             <div className="flex items-center justify-center h-full">
                 <p className="text-3xl font-bold text-black opacity-100">{progress}%</p>
             </div>
