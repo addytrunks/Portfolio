3fd94cf89ff976b19f75a892750a68188903bc87
Adhithya Srivatsan <addytrunks@gmail.com>
2023-12-09 22:16:11 +0530

    feat: change progress text size

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -29,7 +29,7 @@ const Skill = ({directionLeft,imageUrl,progress,noAnimate}:SkillProps) => {
         />
         <div className="absolute opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-white/50 h-10 w-10 xl:w-24 xl:h-24 rounded-full z-0">
             <div className="flex items-center justify-center h-full">
-                <p className="text-3xl font-bold text-black opacity-100">{progress}%</p>
+                <p className="xl:text-3xl text-sm font-bold text-black opacity-100">{progress}%</p>
             </div>
         </div>
     </div>
